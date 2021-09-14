9ab2cb2be0b0049ca88dfdcde3e6a49b5f9311b2
Kangkai YE <kaye@e1r9p3.42.fr>
2021-09-14 19:19:19 +0200

    vector: capacity: ...

--- containers/vector/main.cpp
+++ containers/vector/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 19:37:27 by kaye              #+#    #+#             */
-/*   Updated: 2021/09/13 19:57:43 by kaye             ###   ########.fr       */
+/*   Updated: 2021/09/14 19:17:17 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,56 @@ void vecMaxSizeTest(void) {
 	std::cout << std::endl;
 }
 
+void vecResizeTest(void) {
+{
+	using namespace std;
+
+	vector<int> vec(10, 0);
+
+	cout << "-- real --" << endl;
+
+	cout << "size: " << vec.size() << endl;
+	cout << "cap: " << vec.capacity() << endl;
+
+	vec.resize(1, 10);
+	cout << "--> resize 1, 10" << endl;
+
+	cout << "size: " << vec.size() << endl;
+	cout << "cap: " << vec.capacity() << endl;
+	
+	vec.resize(20, 100);
+	cout << "--> resize 20, 100" << endl;
+
+	cout << "size: " << vec.size() << endl;
+	cout << "cap: " << vec.capacity() << endl;
+
+	cout << endl;
+
+}
+{
+	using namespace ft;
+
+	vector<int> vec(10, 0);
+
+	std::cout << "-- my --" << std::endl;
+
+	std::cout << "size: " << vec.size() << std::endl;
+	std::cout << "cap: " << vec.capacity() << std::endl;
+
+	vec.resize(1, 10);
+	std::cout << "--> reserve 1" << std::endl;
+
+	std::cout << "size: " << vec.size() << std::endl;
+	std::cout << "cap: " << vec.capacity() << std::endl;
+	
+	vec.resize(20, 100);
+	std::cout << "--> reserve 20" << std::endl;
+
+	std::cout << "size: " << vec.size() << std::endl;
+	std::cout << "cap: " << vec.capacity() << std::endl;
+}
+}
+
 void vecReserveTest(void) {
 {
 	using namespace std;
@@ -123,64 +173,286 @@ void vecReserveTest(void) {
 }
 }
 
-// void test(void) {
-// {
-// 	using namespace std;
+void vecAccess(void) {
+	std::cout << "-- Max_size --" << std::endl;
+{
+	using namespace std;
 
-// 	vector<int> vec(10);
-
-// 	cout << "-- real --" << endl;
-
-// 	cout << "size: " << vec.size() << endl;
-// 	cout << "cap: " << vec.capacity() << endl;
-
-// 	vec.reserve(1);
-// 	cout << "reserve 1" << endl;
-
-// 	cout << "size: " << vec.size() << endl;
-// 	cout << "cap: " << vec.capacity() << endl;
+	vector<int> vec(10, 100);
 	
-// 	vec.reserve(20);
-// 	cout << "reserve 20" << endl;
+	cout << "-- real --" << endl;
+	cout << "[0]: " << vec[0] << endl;
+	cout << "[9]: " << vec[9] << endl;
 
-// 	cout << "size: " << vec.size() << endl;
-// 	cout << "cap: " << vec.capacity() << endl;
+	cout << "at(0): " << vec.at(0) << endl;
+	vec.at(9) = 99;
+	cout << "at(9): " << vec.at(9) << endl;
+	// cout << "at(10): " << vec.at(10) << endl; // exception
 
-// 	// vec.resize(10);
-// 	// cout << "size: " << vec.size() << endl;
-// 	// cout << "cap: " << vec.capacity() << endl;
-// 	// cout << "element [5]: " << vec[5] << endl;
+	cout << "front: " << vec.front() << endl;
+	cout << "back: " << vec.back() << endl;
 
-// 	// vec.reserve(20);
-// 	// cout << "size: " << vec.size() << endl;
-// 	// cout << "cap: " << vec.capacity() << endl;
-// 	// cout << "element [15]: " << vec[15] << endl;
+	cout << endl;
+}
+{
+	using namespace ft;
 
-// 	cout << endl;
+	vector<int> vec(10, 100);
+	
+	std::cout << "-- my --" << std::endl;
+	std::cout << "[0]: " << vec[0] << std::endl;
+	std::cout << "[9]: " << vec[9] << std::endl;
 
+	std::cout << "at(0): " << vec.at(0) << std::endl;
+	vec.at(9) = 99;
+	std::cout << "at(9): " << vec.at(9) << std::endl;
+	// std::cout << "at(10): " << vec.at(10) << std::endl; // exception
+
+	std::cout << "front: " << vec.front() << std::endl;
+	std::cout << "back: " << vec.back() << std::endl;
+}
+}
+
+void relationalOperator(void) {
+{
+	using namespace std;
+
+	vector<int> vec(1, 1);
+	vector<int> vec2(1, 2);
+
+	cout << std::boolalpha << "vec == vec2: "<< (vec == vec2) << endl;
+
+	cout << endl;
+}
+// {
+// 	using namespace ft;
+
+// 	vector<int> vec(1, 1);
+// 	vector<int> vec2(1, 2);
+
+// 	std::cout << std::boolalpha << (vec == vec2) << std::endl;
 // }
-// // {
-// // 	using namespace ft;
+}
 
-// // 	vector<int> vec(10);
+void vecAssignTest(void) {
+{
+	using namespace std;
 
-// // 	std::cout << "-- my --" << std::endl;
+	vector<int> vec(10, 999);
+	
+	cout << "-- real --" << endl;
 
-// // 	std::cout << "size: " << vec.size() << std::endl;
-// // 	std::cout << "cap: " << vec.capacity() << std::endl;
+	cout << "size befor assign: " << vec.size() << endl;
+	cout << "cap befor assign: " << vec.capacity() << endl;
 
-// // 	vec.clear();
+	vec.assign(7, 100);
 
-// // 	std::cout << "size: " << vec.size() << std::endl;
-// // 	std::cout << "cap: " << vec.capacity() << std::endl;
-// // }
-// }
+	cout << "size after assign: " << vec.size() << endl;
+	cout << "cap after assign: " << vec.capacity() << endl;
+
+	cout << "[0]: " << vec[0] << endl;
+	cout << "[6]: " << vec[6] << endl;
+
+	cout << endl;
+}
+{
+	using namespace ft;
+
+	vector<int> vec(10);
+
+	std::cout << "-- my --" << std::endl;
+	
+	std::cout << "size befor assign: " << vec.size() << std::endl;
+	std::cout << "cap befor assign: " << vec.capacity() << std::endl;
+
+	vec.assign(7, 100);
+
+	std::cout << "size after assign: " << vec.size() << std::endl;
+	std::cout << "cap after assign: " << vec.capacity() << std::endl;
+
+	std::cout << "[0]: " << vec[0] << std::endl;
+	std::cout << "[6]: " << vec[6] << std::endl;
+}
+}
+
+void vecPushPopTest(void) {
+{
+	using namespace std;
+
+	vector<int> vec(5);
+	
+	cout << "-- real --" << endl;
+
+	cout << "size befor push: " << vec.size() << endl;
+	cout << "cap befor push: " << vec.capacity() << endl;
+
+	vec.push_back(1);
+	vec.push_back(2);
+
+	cout << "size after push: " << vec.size() << endl;
+	cout << "cap after push: " << vec.capacity() << endl;
+	cout << "[5]: " << vec[5] << endl;
+	cout << "[6]: " << vec[6] << endl;
+
+	vec.pop_back();
+	cout << "size after pop: " << vec.size() << endl;
+	cout << "cap after pop: " << vec.capacity() << endl;
+	cout << "last: " << vec.back() << endl;
+
+	cout << endl;
+}
+{
+	using namespace ft;
+
+	vector<int> vec(5);
+	
+	std::cout << "-- my --" << std::endl;
+
+	std::cout << "size befor push: " << vec.size() << std::endl;
+	std::cout << "cap befor push: " << vec.capacity() << std::endl;
+
+	vec.push_back(1);
+	vec.push_back(2);
+
+	std::cout << "size after push: " << vec.size() << std::endl;
+	std::cout << "cap after push: " << vec.capacity() << std::endl;
+	std::cout << "[5]: " << vec[5] << std::endl;
+	std::cout << "[6]: " << vec[6] << std::endl;
+
+	vec.pop_back();
+	std::cout << "size after pop: " << vec.size() << std::endl;
+	std::cout << "cap after pop: " << vec.capacity() << std::endl;
+	std::cout << "last: " << vec.back() << std::endl;
+}
+}
+
+void vecSwapTest(void) {
+{
+	using namespace std;
+
+	vector<int> vec(5, 100);
+	vector<int> vec2(10, 999);
+
+	// std::cout << "address: " << &vec << std::endl;
+	// std::cout << "address: " << &vec2 << std::endl;
+
+	std::cout << "-- real --" << std::endl;
+
+	std::cout << "size before swap: vec: " << vec.size() << std::endl;
+	std::cout << "cap before swap: vec: " << vec.capacity() << std::endl;
+	std::cout << "first: " << vec.front() << std::endl;
+	std::cout << "back: " << vec.back() << std::endl;
+
+	std::cout << "size before swap: vec2: " << vec2.size() << std::endl;
+	std::cout << "cap before swap: vec2: " << vec2.capacity() << std::endl;
+	std::cout << "first: " << vec2.front() << std::endl;
+	std::cout << "back: " << vec2.back() << std::endl;
+
+	cout << endl;
+
+	vec.swap(vec2);
+
+	std::cout << "size after swap: vec: " << vec.size() << std::endl;
+	std::cout << "cap after swap: vec: " << vec.capacity() << std::endl;
+	std::cout << "first: " << vec.front() << std::endl;
+	std::cout << "back: " << vec.back() << std::endl;
+
+	std::cout << "size after swap: vec2: " << vec2.size() << std::endl;
+	std::cout << "cap after swap: vec2: " << vec2.capacity() << std::endl;
+	std::cout << "first: " << vec2.front() << std::endl;
+	std::cout << "back: " << vec2.back() << std::endl;
+
+	cout << endl;
+}
+{
+	using namespace ft;
+
+	vector<int> vec(5, 100);
+	vector<int> vec2(10, 999);
+
+	// std::cout << "address: " << &vec << std::endl;
+	// std::cout << "address: " << &vec2 << std::endl;
+
+	std::cout << "-- my --" << std::endl;
+
+	std::cout << "size before swap: vec: " << vec.size() << std::endl;
+	std::cout << "cap before swap: vec: " << vec.capacity() << std::endl;
+	std::cout << "first: " << vec.front() << std::endl;
+	std::cout << "back: " << vec.back() << std::endl;
+
+	std::cout << "size before swap: vec2: " << vec2.size() << std::endl;
+	std::cout << "cap before swap: vec2: " << vec2.capacity() << std::endl;
+	std::cout << "first: " << vec2.front() << std::endl;
+	std::cout << "back: " << vec2.back() << std::endl;
+
+	std::cout << std::endl;
+
+	vec.swap(vec2);
+
+	std::cout << "size after swap: vec: " << vec.size() << std::endl;
+	std::cout << "cap after swap: vec: " << vec.capacity() << std::endl;
+	std::cout << "first: " << vec.front() << std::endl;
+	std::cout << "back: " << vec.back() << std::endl;
+
+	std::cout << "size after swap: vec2: " << vec2.size() << std::endl;
+	std::cout << "cap after swap: vec2: " << vec2.capacity() << std::endl;
+	std::cout << "first: " << vec2.front() << std::endl;
+	std::cout << "back: " << vec2.back() << std::endl;
+}
+}
+
+void vecAssignment(void) {
+{
+	std::cout << "-- real --" << std::endl;
+
+	using namespace std;
+
+	vector<int> vec(4, 2);
+	vector<int> vec2(vec);
+	vec.pop_back();
+
+	// vec2 = vec;
+
+	std::cout << "vec: " << vec[0] << " - cap: " << vec.capacity() << std::endl;
+	std::cout << "vec2: " << vec2[0] << " - cap: " << vec2.capacity() << std::endl;
+
+	vec[0] = 1;
+	std::cout << "vec: " << vec[0] << " - cap: " << vec.capacity() << std::endl;
+	std::cout << "vec2: " << vec2[0] << " - cap: " << vec2.capacity() << std::endl;
+
+	std::cout << std::endl;
+}
+{
+	std::cout << "-- my --" << std::endl;
+
+	using namespace ft;
+
+	vector<int> vec(4, 2);
+	vector<int> vec2(vec);
+	vec.pop_back();
+
+	// vec2 = vec;
+
+	std::cout << "vec: " << vec[0] << " - cap: " << vec.capacity() << std::endl;
+	std::cout << "vec2: " << vec2[0] << " - cap: " << vec2.capacity() << std::endl;
+
+	vec[0] = 1;
+	std::cout << "vec: " << vec[0] << " - cap: " << vec.capacity() << std::endl;
+	std::cout << "vec2: " << vec2[0] << " - cap: " << vec2.capacity() << std::endl;;
+}
+}
 
 int main(void) {
 	// vecSizeTest();
 	// vecMaxSizeTest();
-	vecReserveTest();
-	// test();
+	// vecResizeTest();
+	// vecReserveTest();
+	// vecAccess();
+	// relationalOperator();
+	// vecAssignTest();
+	// vecPushPopTest();
+	// vecSwapTest();
+	vecAssignment();
 
 	return 0;
 }
--- containers/vector/vector.hpp
+++ containers/vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 14:04:16 by kaye              #+#    #+#             */
-/*   Updated: 2021/09/13 19:57:53 by kaye             ###   ########.fr       */
+/*   Updated: 2021/09/14 19:18:48 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 
 namespace ft {
 	
+	/**
+	 * @class vector
+	 * @brief vectors are sequence containers representing arrays that can change in size.
+	 */
 	template < class T, class Alloc = std::allocator<T> >
 	class vector {
 		public:
@@ -91,7 +95,7 @@ namespace ft {
 			 */
 			explicit vector(const allocator_type & alloc = allocator_type()) :
 				_alloc(alloc),
-				_start(NULL),
+				_begin(NULL),
 				_end(NULL),
 				_capacity(NULL) {}
 
@@ -104,12 +108,18 @@ namespace ft {
 			 */
 			explicit vector(size_type n, const value_type & val = value_type(), const allocator_type & alloc = allocator_type()) :
 				_alloc(alloc),
-				_start(NULL),
+				_begin(NULL),
 				_end(NULL),
 				_capacity(NULL) {
-					_start = _alloc.allocate(n);
-					_end = _start;
-					_capacity = _start + n;
+					try {
+						_begin = _alloc.allocate(n);
+					}
+					catch (std::length_error &e) {
+						throw std::length_error("vector");
+					}
+
+					_end = _begin;
+					_capacity = _begin + n;
 
 					for (; n != 0; n--)
 						_alloc.construct(_end++, val);
@@ -131,7 +141,15 @@ namespace ft {
 			 * @param x: another vector object of the same type, whose contents are either copied or acquired.
 			 */
 			vector(const vector & x) {
-				*this = x;
+				size_type n = x.size();
+
+				this->_begin = this->_alloc.allocate(n);
+				this->_end = this->_begin;
+				this->_capacity = this->_end + n;
+				
+				pointer tmp = x._begin;
+				for (; n != 0; n--)
+					this->_alloc.construct(this->_end++, *tmp++);
 			}
 			
 			/**
@@ -140,7 +158,7 @@ namespace ft {
 			 */
 			~vector(void) {
 				clear();
-				_alloc.deallocate(_start, capacity());
+				_alloc.deallocate(_begin, capacity());
 			}
 
 			/**
@@ -148,15 +166,18 @@ namespace ft {
 			 * @note copies all the elements from x into the container.
 			 * @param x: a vector object of the same type.
 			 */
-			vector &	operator=(vector const & x) {
+			vector &	operator= (vector const & x) {
 				if (this == &x) return *this;
 
-				_alloc.clear();
-				_alloc = x._alloc;
-				_start = x._start;
-				_end = x._end;
-				_capacity = x._capacity; // capacity no change when assign
-				// need insert
+				clear();
+
+				size_type n = x.size();
+				if (n > this->capacity())
+					reserve(n);
+				
+				pointer tmp = x._begin;
+				for (; n != 0; n--)
+					this->_alloc.construct(this->_end++, *tmp++);
 				return *this;
 			}
 
@@ -170,21 +191,25 @@ namespace ft {
 		/* capacity */
 
 			/**
-			 * @brief returns the number of elements in the vector.
+			 * @brief return size.
+			 * @note returns the number of elements in the vector.
 			 * @return the number of elements in the container. 
 			 */
-			size_type	size(void) const { return _end - _start; }
+			size_type	size(void) const { return _end - _begin; }
 
 			/**
-			 * @brief return the maximum number of elements that the vector can hold.
+			 * @brief return maximum size.
+			 * @note return the maximum number of elements that the vector can hold.
 			 * @return the maximum number of elements a vector container can hold as content.
 			 */
 			size_type	max_size(void) const { return allocator_type().max_size(); }
 
 			/**
-			 * @brief resizes the container so that it contains n elements
+			 * @brief change size.
+			 * @note resizes the container so that it contains n elements
 			 * @param n: new container size, expressed in number of elements.
 			 * @param val: object whose content is copied to the added elements in case that n is greater than the current container size. If not specified, the default constructor is used instead.
+			 * @exception throw length_error exception
 			 */
 			void		resize(size_type n, value_type val = value_type()) {
 				if (n > max_size())
@@ -195,24 +220,50 @@ namespace ft {
 						_alloc.destroy(--_end);
 				}
 				else {
-					// comming soon ..
+					if (n < capacity()) {
+						size_type currentSize = size();
+
+						for (; n != currentSize; n--)
+							_alloc.construct(_end++, val);
+					}
+					else {
+						pointer oldStart = _begin;
+						pointer oldEnd = _end;
+						size_type oldN = size();
+						size_type oldCap = capacity();
+
+						_begin = _alloc.allocate(n);
+						_end = _begin;
+						_capacity = _begin + n;
+						for (size_type i = 0; i < n; i++) {
+							if (oldStart != oldEnd)
+								_alloc.construct(_end++, *oldStart++);
+							else
+								_alloc.construct(_end++, val);
+						}
+						_alloc.deallocate(oldStart - oldN, oldCap);
+					}
 				}
 			}
 
 			/**
-			 * @brief returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
+			 * @brief return size of allocated storage capacity.
+			 * @note returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
 			 * @return the size of the currently allocated storage capacity in the vector, measured in terms of the number elements it can hold.
 			 */
-			size_type	capacity(void) const { return _capacity - _start; }
+			size_type	capacity(void) const { return _capacity - _begin; }
 
 			/**
-			 * @brief return whether the vector is empty.
+			 * @brief test whether vector is empty.
+			 * @note return whether the vector is empty.
 			 * @return true if the container size is 0, false otherwise.
 			 */
-			bool		empty(void) const;
+			bool		empty(void) const { return size() == 0; }
 
 			/**
-			 * @brief requests that the vector capacity be at least enough to contain n elements.
+			 * @brief request a change in capacity.
+			 * @note requests that the vector capacity be at least enough to contain n elements.
+			 * @exception length_error exception
 			 * @param n: minimum capacity fot the vector.
 			 */
 			void		reserve(size_type n) {
@@ -220,14 +271,14 @@ namespace ft {
 					throw std::length_error("vector");
 
 				if (n > capacity()) {
-					pointer oldStart = _start;
+					pointer oldStart = _begin;
 					pointer oldEnd = _end;
 					size_type oldN = size();
 					size_type oldCap = capacity();
 
-					_start = _alloc.allocate(n);
-					_end = _start;
-					_capacity = _start + n;
+					_begin = _alloc.allocate(n);
+					_end = _begin;
+					_capacity = _begin + n;
 					while (oldStart != oldEnd)
 						_alloc.construct(_end++, *oldStart++);
 					_alloc.deallocate(oldStart - oldN, oldCap);
@@ -236,24 +287,146 @@ namespace ft {
 			
 		/* element access */
 
-			reference			operator[](size_type n);
-			const_reference		operator[](size_type n) const;
-			reference			at(size_type n);
-			const_reference		at(size_type n) const;
-			reference			front(void);
-			const_reference		front(void) const;
-			reference			back(void);
-			const_reference		back(void) const;
+			/** 
+			 * @brief access element.
+			 * @note returns a reference to the element at position n in the vector container.
+			 * @param n: position of an element in the container.
+			 * @return the element at the specified position in the vector.
+			 */
+			reference			operator[](size_type n) { return *(_begin + n); }
+			const_reference		operator[](size_type n) const { return *(_begin + n); }
+
+			/**
+			 * @brief access element.
+			 * @note returns a refenrece to the elelment at position n in the vector.
+			 * @param n: position of an element in the container.
+			 * @exception throw out_of_range exception
+			 * @return the element at the specified position in the container.
+			 */
+			reference			at(size_type n) {
+				if (n >= size())
+					throw std::out_of_range("vector");
+				return (*this)[n];
+			}
+			const_reference		at(size_type n) const {
+				if (n >= size())
+					throw std::out_of_range("vector");
+				return (*this)[n];
+			}
+			
+			/**
+			 * @brief access first element.
+			 * @note returns a reference to the first element in the vector.
+			 * @return a reference to hte first element in the vector container. 
+			 */
+			reference			front(void) { return *_begin; }
+			const_reference		front(void) const { return *_begin; }
+
+			/**
+			 * @brief access last element.
+			 * @note returns a reference to the last element in the vector.
+			 * @return a reference to the last element in the vector. 
+			 */
+			reference			back(void) { return *(_end - 1); }
+			const_reference		back(void) const { return *(_end - 1); }
 
 		/* modifiers */
+			
+			/** @brief assign vector content.
+			 * @note assigns new contents to the vector, replacing its current contents, and modifying its size accordingly.
+			 *
+			 * @remarks range version: the new contents are elements constructed from each of the elements in the range between first, in the same order.
+			 * @param first: input iterators to the initial positions in a sequence.
+			 * @param last: input iterators to the final positions in a sequence.
+			 * 
+			 * @remarks fill version: the new contents are n elements, each initialized to a copy of val.
+			 * @param n: new size for the container.
+			 * @param val: value to fill the container with. 
+			 */
+			// template <class InputIterator>
+			// void assign (InputIterator first, InputIterator last);
+			void assign(size_type n, const value_type & val) { resize(n, val); }
+
+			/**
+			 * @brief add element at the end.
+			 * @note add a new element at the end of the vector, after its current last element.
+			 * @param val: value to be copied to the new element.
+			 */
+			void push_back(const value_type & val) {
+				if (_end == _capacity)
+					reserve(size() > 0 ? size() * 2 : 1);
+				_alloc.construct(_end++, val);
+			}
+
+			/**
+			 * @brief delete last element.
+			 * @note removes the last element in the vector, effectively reducing the container size by one.
+			 */
+			void pop_back(void) {
+				_alloc.destroy(--_end);
+			}
+
+			/** 
+			 * @brief insert elements.
+			 * @note the vector is extended by inserting new elements before the element at the specified position, effectively increasing the container size by the number of elements inserted.
+			 * @param position: position in the vector where the new elements are inserted.
+			 * @param val: value to be copied(or moved) to the inserted elements.
+			 * @param first, last: iterators specifying a range of elements. Copies of the elements in the range [first, last] are inserted at position(in the same order).
+			 * @return an iterator that points to the first of the newly inserted elements.
+			 */
+			// iterator insert(iterator positionm const value_type & val);
+			// void insert(iterator positionm size_type n, const value_type & val);
+			// template < class InputIterator >
+			// void insert(iterator positionm InputIterator first, InputIterator last);
+
+			/** 
+			 * @brief erase elements.
+			 * @note removes from the vector either a single element or a range of elements.
+			 * @param position: iterator pointing to a single element to be removed from the vector.
+			 * @param first, last: Iterators specifying a range within the vector to be removed: [first, last].
+			 * @return an iterator pointing to the new location of the element that followed the last element erased by the function call.
+			 */
+			// iterator erase(iterator position);
+			// iterator erase(iterator first, iterator last);
+
+			/** 
+			 * @brief swap content.
+			 * @note exchanges the content of the container by the content of x, which is another vector object of the same type.
+			 * @param x: another vector container of the same type.
+			 */
+			void swap(vector & x) {
+				pointer beginTmp = _begin;
+				pointer endTmp = _end;
+				pointer capacityTmp = _capacity;
+				allocator_type allocTmp = _alloc;
+
+				_begin = x._begin;
+				_end = x._end;
+				_capacity = x._capacity;
+				_alloc = x._alloc;
+
+				x._begin = beginTmp;
+				x._end = endTmp;
+				x._capacity = capacityTmp;
+				x._alloc = allocTmp;
+			}
 
 			/** @brief removes all elements from the vector, leaving the container with a size of 0. */
 			void	clear(void) {
 				size_type len = size();
 
-				for (size_type i = 0; i < len; i++)
+				for (size_type i = 0; i < len; i++) {
 					_alloc.destroy(--_end);
+				}
 			}
+
+		/* allocator */
+
+			/** 
+			 * @brief returns a copy of the allocator object associated with the vector;
+			 * @return the allocator.
+			 */
+			allocator_type get_allocator() const { return allocator_type(); }
 		
 		private:
 		/**
@@ -261,10 +434,35 @@ namespace ft {
 		 */
 
 			allocator_type	_alloc;
-			pointer			_start;
+			pointer			_begin;
 			pointer			_end;
 			pointer			_capacity;
 	};
+
+	/**
+	 * non-member function
+	 */
+	
+	template < class T, class Alloc >
+	bool operator== (const vector<T, Alloc> & lhs, const vector<T, Alloc> & rhs); // ==
+
+	template <class T, class Alloc>
+	bool operator!= (const vector<T,Alloc> & lhs, const vector<T,Alloc> & rhs); // !==
+
+	template <class T, class Alloc>
+	bool operator< (const vector<T,Alloc> & lhs, const vector<T,Alloc> & rhs); // <
+
+	template <class T, class Alloc>
+	bool operator<= (const vector<T,Alloc> & lhs, const vector<T,Alloc> & rhs); // <=
+
+	template <class T, class Alloc>
+	bool operator> (const vector<T,Alloc> & lhs, const vector<T,Alloc> & rhs); // >
+
+	template <class T, class Alloc>
+	bool operator>= (const vector<T,Alloc> & lhs, const vector<T,Alloc> & rhs); // >=
+
+	template <class T, class Alloc>
+	void swap (vector<T,Alloc> & x, vector<T,Alloc> & y);
 }
 
 #endif
